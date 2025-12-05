854e54646154bd6d3f895d99d91e20d5d81a7cdc
Morgan Dean <morgandeanhi@gmail.com>
2025-12-05 12:02:19 -0800

    Revert "Update website"
    This reverts commit 2dd3cb8564da077e34a04d60424e6524996a831a.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/dsci320-project-website/" : "/",
+export default defineConfig({
+  base: "/dsci320-project-host",
   build: {
     outDir: "docs",
   },
   plugins: [svelte(), tailwindcss()],
-}));
+});
