2dd3cb8564da077e34a04d60424e6524996a831a
Morgan Dean <morgandeanhi@gmail.com>
2025-12-05 11:59:00 -0800

    Update website

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/dsci320-project-host",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/dsci320-project-website/" : "/",
   build: {
     outDir: "docs",
   },
   plugins: [svelte(), tailwindcss()],
-});
+}));
